251a8cf1c2f0df11b53dbe74dbee1c329778ac92
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-06-06 09:48:16 -0700

    finished model

---
+++ models.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const blogSchema = mongoose.Schema({
+  title:{type:String, required:true },
+  content:{type:String, required:true},
+  author:{
+    firstName:{type:String, required:true},
+    lastName:{type:String, required:true}
+  }
+});
+
+blogSchema.virtual('fullName').get(function(){
+  return `${this.author.firstName} ${this.author.lastName}`.trim();
+});
+
+blogSchema.methods.apiRepr = function(){
+  return {
+    title: this.title,
+    content: this.content,
+    fullName: this.fullName
+  };
+};
+
+const Blog = mongoose.model('Blog',blogSchema);
+module.exports =  Blog;
